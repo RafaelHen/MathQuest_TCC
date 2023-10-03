22c9b8fe176f1a290c49aa29f225c0fdc09209e9
rafael.lopes <rafael.lopes@el.com.br>
2023-10-02 21:29:51 -0300

    Remoção da biblioteca react-modal, criação do componente modal para maior flexibilidade

--- MathQuest_Frontend/src/pages/Professor/Professor.tsx
+++ MathQuest_Frontend/src/pages/Professor/Professor.tsx
@@ -4,9 +4,16 @@ import CreateIcon from '@mui/icons-material/Create';
 import AddIcon from '@mui/icons-material/Add';
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 import SearchIcon from '@mui/icons-material/Search';
-import ModalCadastro from '../../components/ModalCadastro/ModalCadastro';
-function Professor() {
+import   { ModalCadastro }  from '../../components/modal-cadastro/ModalCadastro';
+import { useState } from 'react';
 
+
+function Professor() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+    const handOpenModal = () => {
+      setIsModalOpen(prev => !prev)
+    }
 
     return (<>
         <Menu />
@@ -16,13 +23,14 @@ function Professor() {
                     Gerenciar Jogos 
                 </button>
                 <button className="btnMenu">
-                    <ModalCadastro/>
                     <AddIcon className='icon'/>Cadastrar Turma
                 </button>
                 <button className="btnMenu">
                     <AutoAwesomeIcon className='icon'/>Gerenciar Turma
                 </button>
                 <button className="btnMenu"><SearchIcon className='icon' /> Consultar Desempenho </button>
+                {isModalOpen && <ModalCadastro />}
+                <button onClick={handOpenModal}>Novo</button>
             </div>
         </div>
     </>
