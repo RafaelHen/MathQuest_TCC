04640c0b7f57d3d19e3e621af50781484c7ac36a
RafaelHen <rafaelvictor368@hotmail.com>
2023-10-02 21:36:15 -0300

    Remoção da biblioteca react-modal, criação do componente modal para maior flexibilidade. Correção no commit

---
+++ MathQuest_Frontend/src/components/modal-cadastro/ModalCadastro.tsx
@@ -0,0 +1,18 @@
+import './ModalCadastro.css'
+
+export function ModalCadastro() {
+
+  return (
+    <div className="modal-overlay">
+        <div className="modal-body">
+         <h2>Teste</h2>
+         <form className = "input-container">
+          
+         </form>
+         <button className="btn-secondary">Salvar</button>
+        </div>
+    </div>
+    )
+
+}
+
