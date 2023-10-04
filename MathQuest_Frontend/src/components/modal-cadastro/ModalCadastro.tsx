30bd920367e2ea593344da94b7b2f2d60ccf9107
RafaelHen <rafaelvictor368@hotmail.com>
2023-10-04 00:04:19 -0300

    Aceto modal

--- MathQuest_Frontend/src/components/modal-cadastro/ModalCadastro.tsx
+++ MathQuest_Frontend/src/components/modal-cadastro/ModalCadastro.tsx
@@ -1,7 +1,7 @@
 import './ModalCadastro.css'
 
 
-interface ModalProps{
+interface ModalProps {
   closeModal(): void;
 }
 
@@ -9,35 +9,34 @@ export function ModalCadastro({ closeModal }: ModalProps) {
 
   return (<>
     <div className="modal-overlay">
-        <div className="modal-body">
-          <div className="header-modal">
-            <h1> Cadastrar Turma </h1>
-            <button onClick={closeModal} className="close">X</button>
-          </div>
-          <form className = "input-container">
-            <div className="escola">
-              <label htmlFor="email">Nome da Escola: </label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="inputContainer-horizontal">
-              <label htmlFor="turma">
-                Turma
-                <input type="password" name="senha" id="senha" />
-              </label>
-
-              <label htmlFor="serie">
-                Serie
-                <input type="password" name="senha" id="senha" />
-              </label>
-           </div>
-            
-          </form>
-         <button className="btn-salvar">Salvar</button>
+      <div className="modal-body">
+        <div className="header-modal">
+          <h1> Cadastrar Turma </h1>
+          <button onClick={closeModal} className="close">X</button>
         </div>
+        <form className="input-container">
+          <div className="input-container-escola">
+            <label htmlFor="email">Nome da Escola:</label>
+            <input type="text" name="email" id="email" />
+
+          </div>
+          <div className="input-container-turma-serie">
+            <div className="input-group">
+              <label htmlFor="turma">Turma:</label>
+              <input type="text" name="turma" id="turma" />
+            </div>
+            <div className="input-group-serie">
+              <label htmlFor="serie">Serie:</label>
+              <input type="text" name="serie" id="serie" />
+            </div>
+          </div>
+        </form>
+        <button className="btn-salvar">Salvar</button>
+      </div>
 
     </div>
-    </>
-    )
+  </>
+  )
 
 }
 
